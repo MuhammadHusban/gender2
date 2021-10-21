d2df4064ffcdac56b126ffc544411715d1a77b0e
MuhammadHusban <92823901+MuhammadHusban@users.noreply.github.com>
2021-10-21 02:42:34 -0700

    jugari tareeqa

---
+++ assigment # 2/MarkSheet.py
@@ -0,0 +1,17 @@
+ (Mark sheet)
+
+
+percent = 72
+if percent <= 100 and percent > 80: 
+    print("Grade A+")
+elif percent <= 80 and percent > 70:
+    print("Grade A")
+elif percent <= 70 and percent > 60:
+    print("Grade B")
+elif percent <= 60 and percent > 50:
+    print("Grade C")
+elif percent <= 50 and percent > 40:
+    print("Grade D")
+else:
+    percent <= 40
+    print("Fail")
